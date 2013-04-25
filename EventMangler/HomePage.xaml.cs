b7fc3e78a373dcbff061054becaff2f473a38f48
ThaShepherd <matthew.j.alioto@gmail.com>
2013-04-24 23:44:06 -0400

    Initial commit

---
+++ EventMangler/HomePage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using EventMangler.model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace EventMangler
+{
+
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        ImageLibrary imageLib;
+
+        public HomePage()
+        {
+            InitializeComponent();
+            imageLib = new ImageLibrary(EventMangler.Properties.Resources.default_imagelist);
+            foreach (var imageList in imageLib.getImageLists())
+            {
+                // Create label with name of imageList
+                Label imageListLabel = new Label();
+                imageListLabel.Content = imageList.Key;
+
+                // Add the label to the vertical stack
+                ImageListStack.Children.Add(imageListLabel);
+
+                // Create horizontal stack panel to place images in
+                StackPanel listPanel = new StackPanel();
+                listPanel.Background = new SolidColorBrush(Colors.Transparent);
+                listPanel.Orientation = Orientation.Horizontal;
+                listPanel.Height = 108;
+                listPanel.AllowDrop = true;
+                listPanel.Drop += new DragEventHandler(
+                    delegate(object sender, DragEventArgs e)
+                    {
+                        // Handle image drop
+                        e.Handled = true;
+
+                        // Check for files in the hovering data object.                        
+                        if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+                        {
+                            string[] fileNames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                            if (fileNames != null)
+                            {
+                                foreach (string filename in fileNames)
+                                {
+                                    System.Console.WriteLine("Image dropped: " + filename);
+
+                                    // Construct image from path
+                                    FTLImage newImage = imageLib.addImageFromPath(filename);
+
+                                    // Border & style image
+                                    Border newImageBorder = getBorderedImage(newImage);
+
+                                    // Add to stack panel
+                                    listPanel.Children.Add(newImageBorder);
+                                }
+                            }
+                        }
+                    });
+
+                // Add the horizontal stack to the vertical stack
+                ImageListStack.Children.Add(listPanel);
+
+                // Add listed images to the horizontal stack
+                foreach (FTLImage image in imageList.Value) listPanel.Children.Add(getBorderedImage(image));
+            };
+        }
+
+        Border getBorderedImage(FTLImage image)
+        {
+            // Create image source
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            if (File.Exists(Properties.Resources.mod_root_dir + "/img/" + image.path))
+            {
+                src.UriSource = new Uri(Properties.Resources.mod_root_dir + "/img/" + image.path, UriKind.Absolute);
+            }
+            else if (File.Exists(Properties.Resources.resources_root_dir + "/img/" + image.path))
+            {
+                src.UriSource = new Uri(Properties.Resources.resources_root_dir + "/img/" + image.path, UriKind.Absolute);
+            }
+            else
+            {
+                src.UriSource = new Uri("img/nofile.png", UriKind.Relative);
+            }
+            src.EndInit();
+
+            Image img = new Image();
+            img.Margin = new Thickness(5);
+            img.Source = src;
+
+            // Create & style border around image
+            Border border = new Border();
+            border.BorderBrush = new SolidColorBrush(Colors.White);
+            border.BorderThickness = new Thickness(1);
+            border.Margin = new Thickness(10);
+            border.CornerRadius = new CornerRadius(10);
+            border.Background = new SolidColorBrush(Colors.Black);
+            border.Child = img;
+            //border.Style = Resources["imageBorderStyle"] as Style;
+            return border;
+        }
+    }
+}
---
+++ EventMangler/model/ImageLibrary.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Xml.Linq;
+
+namespace EventMangler.model
+{
+    class ImageLibrary
+    {
+        private Dictionary<string, List<FTLImage>> imageLists;
+
+        public ImageLibrary(string path)
+        {
+            imageLists = getImageLists(path);
+        }
+
+        public Dictionary<string, List<FTLImage>> getImageLists()
+        {
+            return imageLists;
+        }
+
+        public FTLImage addImageFromPath(string imageFilePath, string imageList = "PLANET_CLOSE")
+        {
+            if (File.Exists(imageFilePath))
+            {
+                // Move image at imageFilePath to mod image directory
+                string filename = Path.GetFileName(imageFilePath);
+                string destFolder = Path.Combine(Properties.Resources.mod_root_dir, "img/stars/");
+                File.Copy(imageFilePath, Path.Combine(destFolder, filename), false);
+                
+                // Get source for deriving image dimensions
+                BitmapImage src = new BitmapImage();
+                src.BeginInit();
+                src.UriSource = new Uri(Path.Combine(destFolder, filename), UriKind.Absolute);
+                src.EndInit();
+
+                // Create new FTLImage
+                FTLImage newImage = new FTLImage(Path.Combine("stars/", filename), (int) src.Width, (int) src.Height);
+                imageLists[imageList].Add(newImage);
+
+                // Add XML element to events_imagelist
+                System.Console.WriteLine(newImage.toXElement().ToString());
+                return newImage;
+            } else throw new FileNotFoundException();            
+        }
+
+        protected Dictionary<string, List<FTLImage>> getImageLists(string eventFilePath)
+        {
+            // Display the passed filepath
+            System.Console.WriteLine("Loading imageLists from " + eventFilePath);
+            string xmlString = File.ReadAllText(eventFilePath);
+            XDocument events_imageList = XDocument.Parse("<imageLists>" + xmlString + "</imageLists>");
+
+            Dictionary<string, List<FTLImage>> imageLists = new Dictionary<string, List<FTLImage>>();
+            var imageListQuery =
+                from
+                    il in events_imageList.Descendants("imageList")
+                select il;
+            foreach (var imageList in imageListQuery)
+            {
+                var imageQuery =
+                    from
+                        i in imageList.Descendants("img")
+                    select i;
+                List<FTLImage> images = new List<FTLImage>();
+                foreach (var image in imageQuery) images.Add(new FTLImage(
+                        image.Value, // Image path
+                        (int)image.Attribute("w"),
+                        (int)image.Attribute("h")));
+                imageLists.Add(imageList.Attribute("name").Value, images);
+            }
+            foreach (string list in imageLists.Keys) Console.WriteLine(list);
+            return imageLists;
+        }
+    }
+
+    public struct FTLImage
+    {
+        public readonly int w, h;
+        public readonly string path;
+
+        public FTLImage(string path, int w, int h)
+        {
+            this.path = path;
+            this.w = w;
+            this.h = h;
+        }
+
+        public XElement toXElement()
+        {
+            return new XElement("img", new XAttribute("w", w), new XAttribute("h", h), path);
+        }
+    }
+}
